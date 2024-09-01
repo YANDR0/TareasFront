9e90c6eb80a8f5b2c5d0f3997c694aed05927f85
Yael <yael.rodriguez@iteso.mx>
2024-09-01 17:14:29 -0600

    Ya mero

--- Tarea2/src/scripts/main.ts
+++ Tarea2/src/scripts/main.ts
@@ -1,31 +1,60 @@
 
-async function fetchData() {
+async function dynamicData() {
     const dir = '../assets/jsons/data.json';
     const res = await fetch(dir);
-    const data = await res.json()
-    return data
+    const data = await res.json();
+
+    // Creación portada
+    var tmpData = data.Portada
+    var actualPage = document.getElementsByClassName('Portada')[0];
+    var divPortada = document.createElement("div");
+    
+    for(let i in tmpData){
+        let hs = document.createElement("h2");
+        let text = document.createTextNode(i);
+        hs.appendChild(text);
+        divPortada.appendChild(hs);
+
+        for(let j = 0; j < tmpData[i].length; j++){
+            let ps = document.createElement("p");
+            text = document.createTextNode(tmpData[i][j]);
+            ps.appendChild(text);
+            divPortada.appendChild(ps);
+        }
+    }
+    actualPage.appendChild(divPortada);
+
+
+    // Creacion info
+    tmpData = data.Info;
+    actualPage = document.getElementsByClassName('Info')[0];
+
+    for(let i = 0; i < tmpData.length; i++){
+        let key = Object.keys(tmpData[i])[0];
+        let divs = document.createElement("div");
+        let hs = document.createElement("h2");
+        let text = document.createTextNode(key);
+        let uls = document.createElement("ul");
+        hs.appendChild(text);
+        divs.appendChild(hs);
+        
+        for (let j = 0; j < tmpData[i][key].length; j++){
+            let lis = document.createElement("li");
+            text = document.createTextNode(tmpData[i][key][j]);
+            lis.appendChild(text);
+            uls.appendChild(lis)
+        }
+
+        divs.appendChild(uls);
+        actualPage.appendChild(divs)
+    }
+
+
+    //Creacion pie
+
 }
 
-function createPortada(){
-
-}
-
-function createInfo(){
-
-}
-
-function createFooter(){
-
-}
-
-const data = fetchData();
-createPortada();
-createInfo();
-createFooter();
-
-
-
-
+dynamicData()
 
 
 
